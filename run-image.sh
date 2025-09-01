d6270f9caaa5f8dbd6a130dac79ffb146777936b
jiashhu <hjs16@tsinghua.org.cn>
2025-09-01 22:41:13 +0800

    Update all files

--- run-image.sh
+++ run-image.sh
@@ -1,14 +1,15 @@
-#!/bin/bash
+#!/bin/zsh
 
 # 镜像名字
 IMAGE_NAME="my-ngsxfem"
 
-# 如果第一个参数是 "build"，则构建镜像
-if [ "$1" == "build" ]; then
+# 如果第一个参数是 "build"，则构建镜像，首次运行时需要构建，运行 ./sh run-image.sh build
+if [ "$1" = "build" ]; then
     echo "Building Docker image ${IMAGE_NAME}..."
     docker build -t $IMAGE_NAME .
 fi
 
 # 启动容器
 echo "Running Docker container from image ${IMAGE_NAME}..."
-docker run -it --rm -p 8888:8888 -v "$(pwd)":/home/jovyan/work $IMAGE_NAME
+docker run -it --rm -p 8888:8888 -v "$(pwd)":/home/jovyan/work \
+        -e PYTHONPATH=/home/jovyan/work/ngs-esfem:$PYTHONPATH $IMAGE_NAME
---
+++ update_submod.sh
@@ -0,0 +1,5 @@
+git add -A
+git commit -m " Update all files "
+git add ngs-esfem
+git commit -m " Update ngs-esfem submodule to latest commit "
+git push
